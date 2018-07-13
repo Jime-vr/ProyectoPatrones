6bb5832ccc1dab536175170827abd591044ba328
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2018-07-13 15:43:52 -0600

    Division de gestores y metodos de Usuario.

---
+++ ProyectoPatrones/src/com/cenfotec/proyecto/clases/test/GestorUsuarioTest.java
@@ -0,0 +1,40 @@
+package com.cenfotec.proyecto.clases.test;
+
+import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import com.cenfotec.proyecto.gestores.GestorUsuario;
+
+class GestorUsuarioTest {
+	
+	@Test
+	public void validarCorreoTest() throws IOException {
+		GestorUsuario gu = new GestorUsuario();
+		gu.quemarDatosUsuario();
+		assertEquals(false, gu.validarCorreo("silvia@gmail.com"));
+		assertEquals(true, gu.validarCorreo("susana@gmail.com"));
+	}
+	
+	@Test
+	public void obtenerCorreoUsuarioTest()throws IOException{
+		GestorUsuario gu = new GestorUsuario();
+		assertEquals("correo default",gu.obtenerCorreoUsuario());
+	}
+	
+	@Test
+	public void obtenerCorreoUsuarioTest2()throws IOException{
+		GestorUsuario gu = new GestorUsuario();
+		gu.quemarDatosUsuario();
+		gu.iniciarSesion("susana@gmail.com", "741");
+		assertEquals("susana@gmail.com",gu.obtenerCorreoUsuario());
+	}
+	
+	@Test
+	public void obtenerCorreoUsuarioTest3()throws IOException{
+		GestorUsuario gu = new GestorUsuario();
+		gu.quemarDatosUsuario();
+		gu.iniciarSesion("roberto@gmail.com", "7524");
+		assertEquals("correo default",gu.obtenerCorreoUsuario());
+	}
+
+}
